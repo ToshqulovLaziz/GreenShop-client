0c75ec3fd3e61127c93c02aa57906ccb3c5491f0
ToshqulovLaziz <toshqulovlaziz04@gmail.com>
2023-10-09 19:55:06 +0500

    message

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import ReactDOM from 'react-dom/client';
 import "./index.css";
+import App from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<h1>Hello world</h1>
-);
+root.render(<App/>);
