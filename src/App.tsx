e8b230368ac45f65fda2ca0b0a040906a99c38a4
ToshqulovLaziz <toshqulovlaziz04@gmail.com>
2023-10-09 20:16:19 +0500

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-const App = () => {
+import type { FC } from "react";
+const App: FC = () => {
   return (
     <div>
-      <h1>Hello world</h1>
+      <h1>Hello world </h1>
+      <h3>Hello world </h3>
+      <p>Hello world </p>
+      <h4>Hello world </h4>
+      <h6>Hello world </h6>
     </div>
-  )
-}
+  );
+};
 
 export default App;
